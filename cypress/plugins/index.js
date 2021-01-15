611d0a61f5b09d7d25ead1b0dda4fc663e62fdd0
Roshan Jossy <roshan.jossy@jina.ai>
2021-01-15 17:29:36 +0100

    fix: add retina screen sizes configuration for popular browsers

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,19 +16,30 @@
  * @type {Cypress.PluginConfig}
  */
 const { addMatchImageSnapshotPlugin, } = require('cypress-image-snapshot/plugin');
+const retinaScreenWidth = 2800
+const retinaScreenHeight = 2400
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    if (browser.name === 'electron' && browser.isHeadless) {
-      launchOptions.preferences['width'] = 1920;
-      launchOptions.preferences['height'] = 1080;
-      launchOptions.preferences['resizable'] = false;
-      return launchOptions
-    }
+  on('before:browser:launch', (browser, launchOptions) => {
     if (browser.name === 'chrome' && browser.isHeadless) {
-      launchOptions.args.push('--window-size=1920,1080')
-      return launchOptions
+      launchOptions.args.push(`--window-size=${retinaScreenWidth},${retinaScreenHeight}`)
+
+      // force screen to be retina (2800x2400 size)
+      launchOptions.args.push('--force-device-scale-factor=2')
+    }
+
+    if (browser.name === 'electron' && browser.isHeadless) {
+      // fullPage screenshot size is 1400x1200
+      launchOptions.preferences.width = retinaScreenWidth
+      launchOptions.preferences.height = retinaScreenHeight
+    }
+
+    if (browser.name === 'firefox' && browser.isHeadless) {
+      // menubars take up height on the screen
+      // so fullPage screenshot size is 1400x1126
+      launchOptions.args.push('--width=' + retinaScreenWidth)
+      launchOptions.args.push('--height=' + retinaScreenHeight)
     }
   })
 
